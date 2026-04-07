c3bb30b3e956e01c826fe914babc7db4f93ce61b
Konstantin <kolu@DESKTOP-EMJS1H8.localdomain>
2026-04-07 13:34:04 +0200

    remove deleted products from DOM

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML +=
         `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Wednesday, June 15
         </div>
@@ -99,8 +100,10 @@ document.querySelectorAll('.js-delete-quantity-link')
     .forEach((link) => {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
-            console.log(productId)
             removeFromCard(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         })
     })
 
