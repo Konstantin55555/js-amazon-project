f2fe8288ee9beb4271c543167f4e0df63e02fe70
Konstantin <kolu@DESKTOP-EMJS1H8.localdomain>
2026-04-07 13:23:17 +0200

    delete products from cart when clicking delete button

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
 }, {
@@ -23,4 +23,30 @@ export function addToCard(productId) {
             quantity: 1
         });
     }
+}
+
+export function removeFromCard(productId) {
+    let matchingIndex;
+
+    cart.forEach((item, index) => {
+        if (productId === item.productId) {
+            matchingIndex = index;
+        }
+    });
+
+    if (matchingIndex !== -1) {
+        cart.splice(matchingIndex, 1);
+    }
+
+    // //2nd variant
+    // let newCart = [];
+
+    // cart.forEach((item) => {
+    //     if (productId !== item.productId) {
+    //         newCart.push(item);
+    //     }
+    // });
+    // cart = newCart;
+    console.log(cart);
+
 }
